cc4e40e4adb81370296574851b5c42299b69d0fb
minichambre <minichambre95@gmail.com>
2019-03-18 20:51:15 +0000

    added file reading functionality and storing config as object

--- app/src/components/FileLoader/index.jsx
+++ app/src/components/FileLoader/index.jsx
@@ -3,6 +3,7 @@ import styles from './styles.css'
 var app = require('electron').remote;
 var dialog = app.dialog;
 var fs = require('fs');
+var readline = require('readline');
 export default class FileLoader extends Component {
     constructor(props){
       super(props);
@@ -10,18 +11,56 @@ export default class FileLoader extends Component {
         fileLocation: null
       }
       this.openFile = this.openFile.bind(this);
+      this.readFromFile = this.readFromFile.bind(this);
+      this.digestLine = this.digestLine.bind(this);
     }
 
+    /* Opens a dialogue box which allows the user to select the path
+     * of their league of legends input.cfg file.
+     * Function then sets the state to the path of the selected file, and calls a file reader function
+    */
     openFile(){
       dialog.showOpenDialog((filenames) => {
-        if (filenames === undefined){
-          console.log("nothing selected");
+        if (filenames === undefined || filenames.length > 1){
           return;
         }else {
-          this.setState({fileLocation: filenames});
+          this.setState({fileLocation: filenames[0]}, () => this.readFromFile(this.state.fileLocation));
         }
       })
     }
+
+    readFromFile(filePath){
+      var config = new Object();
+      var rd = readline.createInterface({
+        input:fs.createReadStream(filePath),
+        output:process.stdout,
+        console:false
+      });
+
+      //Start reading the file one line at a time
+      rd.on('line', function(line){
+        if (line == "" || line == undefined) {
+          return;
+        }
+        let split = line.split('=');
+        let key = split.shift();
+        let val = "";
+
+        if (split.length > 1){
+          val = split.join();
+        } else {
+          val = split[0];
+        }
+
+        config[key] = val;
+      });
+
+      rd.on('close', () => {
+        console.log(config);
+      })
+    }
+
+
     render() {
         return (
           <div className={styles.container}>
